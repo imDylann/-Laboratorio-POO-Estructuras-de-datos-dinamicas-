481c9056f0da97dddcd02ed57ccf5642212ed518
Dylan Quiros <dyquirosro@est.utn.ac.cr>
2024-08-03 23:09:14 -0600

    Ahora si esta todo xd

--- RegistroEstudiante/RegistroEstudiante.java
+++ RegistroEstudiante/RegistroEstudiante.java
@@ -4,10 +4,34 @@
  */
 package RegistroEstudiante;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author PC
  */
 public class RegistroEstudiante {
-    
+   private ArrayList<Estudiante> estudiantes;
+
+    public RegistroEstudiante() {
+        estudiantes = new ArrayList<Estudiante>();
+    }
+
+    public ArrayList<Estudiante> ListarEstudiantes() {
+        return estudiantes;
+    }
+   
+   public void AgregarEstudiante(Estudiante estudiante){
+       estudiantes.add(estudiante);
+   }
+   
+     public void EliminarEstudiante(Estudiante estudiante){
+       estudiantes.remove(estudiante);
+   }
+
+    @Override
+    public String toString() {
+        return "RegistroEstudiante{" + "estudiantes=" + estudiantes + '}';
+    }
+     
 }
